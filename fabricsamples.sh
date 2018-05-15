329d548af79eb6664f6dd615d033cc4b5f4d4906
Dan <danandreist@gmail.com>
2018-05-15 20:55:34 +0300

    Update fabricsamples.sh

--- fabricsamples.sh
+++ fabricsamples.sh
@@ -1,1 +1,19 @@
-x
+#!/bin/bash
+
+# This will install the fabric samples. Before you run this, make sure you ran the prereqs first.
+
+# Usage  
+# sudo wget -O - https://raw.githubusercontent.com/AndreiD/HyperledgerFabricBootstrap/master/fabricsamples.sh | bash
+
+ #stops the execution of a script if a command or pipeline has an error
+ set -e
+ 
+# let'em have colors
+end="\033[0m"
+red="\033[0;31m"
+green="\033[0;32m"
+
+
+#    echo "${red}Error: Ubuntu ${CODENAME} is not supported${end}"
+ 
+# echo -e "${green}..........Updating Packages..........${end}"
