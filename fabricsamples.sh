437973dbd475e1ad12cd5a7d4912dac79a3c196b
Dan <danandreist@gmail.com>
2018-05-15 21:34:59 +0300

    Update fabricsamples.sh

--- fabricsamples.sh
+++ fabricsamples.sh
@@ -14,6 +14,17 @@ red="\033[0;31m"
 green="\033[0;32m"
 
 
-#    echo "${red}Error: Ubuntu ${CODENAME} is not supported${end}"
- 
-# echo -e "${green}..........Updating Packages..........${end}"
+#echo "${red}Error: Ubuntu ${CODENAME} is not supported${end}"
+#echo -e "${green}..........Updating Packages..........${end}"
+
+echo -e "${green}..........Downloading fabric-samples..........${end}"
+cd $HOME
+
+# if it exists, delete it
+if [ -d "fabric-samples" ]; then
+  rmdir "fabric-samples"
+fi
+
+git clone -b master https://github.com/hyperledger/fabric-samples.git
+cd fabric-samples
+git checkout v.1.1.0
